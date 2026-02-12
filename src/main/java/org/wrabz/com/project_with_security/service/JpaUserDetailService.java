1b21ff674d76cfd7ebed5827830b5408ef925607
Nasasiira Rajjab <nwrabz@gmail.com>
2026-02-13 00:23:04 +0300

    The implementation of the UserDetailsService contract

---
+++ src/main/java/org/wrabz/com/project_with_security/service/JpaUserDetailService.java
@@ -0,0 +1,32 @@
+package org.wrabz.com.project_with_security.service;
+
+import org.jspecify.annotations.NonNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.wrabz.com.project_with_security.CustomUserDetails;
+import org.wrabz.com.project_with_security.model.User;
+import org.wrabz.com.project_with_security.repository.UserRepository;
+
+import java.util.function.Supplier;
+
+@Service
+public class JpaUserDetailService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public CustomUserDetails loadUserByUsername(@NonNull String username) throws UsernameNotFoundException {
+        Supplier<UsernameNotFoundException>  supplier =
+                () -> new UsernameNotFoundException(
+                        "Username not found: " + username
+                );
+
+        var user = userRepository.findUserByUsername(username)
+                .orElseThrow(supplier);
+
+        return new CustomUserDetails(user);
+    }
+}
