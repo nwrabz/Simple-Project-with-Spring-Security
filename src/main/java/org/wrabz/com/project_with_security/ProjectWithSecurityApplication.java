8f3e63d368ce474df03df55e17a85c9fdb829371
Nasasiira Rajjab <nwrabz@gmail.com>
2026-02-12 15:04:55 +0300

    starting the application after dependecy configuration

---
+++ src/main/java/org/wrabz/com/project_with_security/ProjectWithSecurityApplication.java
@@ -0,0 +1,13 @@
+package org.wrabz.com.project_with_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWithSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWithSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/wrabz/com/project_with_security/ProjectWithSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wrabz.com.project_with_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWithSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
