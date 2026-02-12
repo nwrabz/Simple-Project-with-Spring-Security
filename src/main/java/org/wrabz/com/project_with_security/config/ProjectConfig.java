11a1ee96a61961e2180b87eaae22915685f083d8
Nasasiira Rajjab <nwrabz@gmail.com>
2026-02-12 21:42:02 +0300

    Registering a bean for each PasswordEncoder

---
+++ src/main/java/org/wrabz/com/project_with_security/config/ProjectConfig.java
@@ -0,0 +1,26 @@
+package org.wrabz.com.project_with_security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SCryptPasswordEncoder sCryptPasswordEncoder() {
+        return new SCryptPasswordEncoder(
+                65536,  // cpu cost (N)
+                8,      // memory cost (r)
+                1,      // parallelization (p)
+                32,     // key length
+                16      // salt length
+        );
+    }
+}
