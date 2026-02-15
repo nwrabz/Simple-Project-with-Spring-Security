a51b04e91e664a61e6668adfd99e14972b261c18
Nasasiira Rajjab <nwrabz@gmail.com>
2026-02-15 11:11:22 +0300

    Configuring formLogin as the authentication method using securityFilterChain bean

--- src/main/java/org/wrabz/com/project_with_security/config/ProjectConfig.java
+++ src/main/java/org/wrabz/com/project_with_security/config/ProjectConfig.java
@@ -1,11 +1,9 @@
 package org.wrabz.com.project_with_security.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -43,5 +41,18 @@ public class ProjectConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
+    }
+
+    @Bean
+    protected SecurityFilterChain springSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authenticationProvider(authenticationProvider)
+                .formLogin(form ->
+                        form.defaultSuccessUrl("/main", true)
+                )
+                .authorizeHttpRequests(auth ->
+                        auth.anyRequest().authenticated());
+
+        return http.build();
     }
 }
